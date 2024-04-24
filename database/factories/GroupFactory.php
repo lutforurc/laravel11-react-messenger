ea52154f4613a8152deac32466d4ebced3f7ac61
Lutfor <lutforurc@gmail.com>
2024-04-24 16:14:47 +0600

    Initial Stage

---
+++ database/factories/GroupFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Group>
+ */
+class GroupFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name,
+            'description' => $this->faker->sentence,
+            // 'owner_id' => \App\Models\User::factory(),
+            // 'last_message_id' => \App\Models\Message::factory(),
+        ];
+    }
+}
---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Group;
+use App\Models\Message;
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+
+        User::factory()->create([
+            'name' => 'John Doe',
+            'email' => 'john@example.com',
+            'password' => bcrypt('password'),
+            'is_admin' => true
+        ]);
+        User::factory()->create([
+            'name' => 'John Doe',
+            'email' => 'user@example.com',
+            'password' => bcrypt('password'),
+            'is_admin' => false
+        ]);
+
+        User::factory(10)->create();
+        for ($i = 0; $i < 5; $i++) {
+            $group = Group::factory()->create([
+                'owner_id' => 1,
+            ]);
+
+            $users = User::isRandomOrder()->limit(rand(2, 5))->pluk('id');
+            $group->users()->attach(array_unique([1, ...$users]));
+        }
+
+        Message::factory(1000)->create();
+        $messages = Message::whereNull('group_id')->orderBy('created_at')->get();
+    }
+}
